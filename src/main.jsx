a3eb501a6e85d60af97ec2513f33708c3ca5be5e
Levan Kopaliani <ciceron23x@gmail.com>
2023-05-01 22:53:08 +0400

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 const router = createBrowserRouter(
   [
     {
-      path: "/*",
+      path: "/",
       element: <App />,
     },
   ],
